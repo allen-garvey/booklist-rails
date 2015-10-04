92b60856b740bb715aa87b91db91b06650cb4176
Allen Garvey <allengarvey@hotmail.com>
2015-10-04 17:28:39 -0400

    add update and edit methods for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,21 @@ class BooksController < ApplicationController
         @book.save
         redirect_to @book
     end
+    def edit
+         @book = Book.find(params[:id])
+         @authors = Author.all
+    end
 
+    def update
+        @book = Book.find(params[:id])
+
+        if @book.update(book_params)
+            redirect_to @book
+        else
+            render 'edit'
+        end
+    end
+    
     private
     def book_params
         params.require(:book).permit(:title, :author_id)
