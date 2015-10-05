ce074c4bcbe9b295ca0f0890b6d2aa1fabf5540e
Allen Garvey <allengarvey@hotmail.com>
2015-10-05 02:02:34 -0400

    add validation for pre_rating and default date_added in book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,18 @@
 class Book < ActiveRecord::Base
+	def self.pre_rating_min
+		1
+	end
+	def self.pre_rating_max
+		100
+	end
+
 	validates :title, presence: true
+	validates :pre_rating, numericality: {only_integer: true, less_than_or_equal_to: Book.pre_rating_max, greater_than_or_equal_to: Book.pre_rating_min}
 	belongs_to :author
+	after_initialize :init
+
+	def init
+		self.date_added ||= Date.today
+	end
+	
 end
