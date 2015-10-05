68cd1aa4474d5ffe0d973d70d7f23ebfb3b96422
Allen Garvey <allengarvey@hotmail.com>
2015-10-05 02:00:12 -0400

    add pre_rating and date_added columns to book model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004040126) do
+ActiveRecord::Schema.define(version: 20151005050008) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "last"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20151004040126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "author_id"
+    t.date     "date_added"
+    t.integer  "pre_rating"
   end
 
   add_index "books", ["author_id"], name: "index_books_on_author_id"
