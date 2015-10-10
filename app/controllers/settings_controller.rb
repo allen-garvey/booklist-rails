2d0dce67225285b26a8e2969edf9f6ee43ea2b8c
Allen Garvey <allengarvey@hotmail.com>
2015-10-10 18:55:51 -0400

    add list_book to settings controller fields

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,5 @@
 class SettingsController < ApplicationController
 	def fields
-		@fields = ['Author', 'Book', 'Classification', 'Genre', 'List', 'Rating']
+		@fields = ['Author', 'Book', 'Classification', 'Genre', 'List', 'List_Book', 'Rating']
 	end
 end
