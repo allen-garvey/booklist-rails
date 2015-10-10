962fb86aa7c289d25d1ad84e642ec7517fa29af8
Allen Garvey <allengarvey@hotmail.com>
2015-10-10 18:34:00 -0400

    create controller for list_book

---
+++ app/helpers/list_book_helper.rb
@@ -0,0 +1,2 @@
+module ListBookHelper
+end
---
+++ test/controllers/list_book_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ListBookControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
