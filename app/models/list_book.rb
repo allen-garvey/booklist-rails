bad2ee3a0bad9d7c7b25e20c5a4f43b50b5ab2cf
Allen Garvey <allengarvey@hotmail.com>
2015-10-10 18:55:35 -0400

    create list_book model

---
+++ app/models/list_book.rb
@@ -0,0 +1,10 @@
+class ListBook < ActiveRecord::Base
+  belongs_to :book
+  belongs_to :list
+  validates :book_id, presence: true
+  validates :list_id, presence: true
+  validates :book_id, uniqueness: {scope: :list_id}
+  def to_s
+  	"#{self.list} #{self.book}"
+  end
+end
