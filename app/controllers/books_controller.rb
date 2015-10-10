e8d81e3897da9a1ef2e7bd7a009b830eb6a53529
Allen Garvey <allengarvey@hotmail.com>
2015-10-09 22:45:44 -0400

    add genre to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < BaseController
     protected
     def model_params
-        params.require(:book).permit(:title, :author_id, :pre_rating, :classification_id)
+        params.require(:book).permit(:title, :author_id, :pre_rating, :classification_id, :genre_id)
     end
     def related_fields
         @authors = Author.all
         @classifications = Classification.all
+        @genres = Genre.all
     end
     def model
         Book
