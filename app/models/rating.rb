f3076b01d6adf9abba7e494d768ea6e812e66271
Allen Garvey <allengarvey@hotmail.com>
2015-10-10 16:38:55 -0400

    add to string and default date added to rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,14 @@
 class Rating < ActiveRecord::Base
-  belongs_to :book
-  validates :post_rating, presence: true
-  validate :book_id, presence: true
-  validates :post_rating, numericality: {only_integer: true, less_than_or_equal_to: Book.pre_rating_max, greater_than_or_equal_to: Book.pre_rating_min}
+	belongs_to :book
+	validates :post_rating, presence: true
+	validates :book_id, presence: true
+	validates :post_rating, numericality: {only_integer: true, less_than_or_equal_to: Book.pre_rating_max, greater_than_or_equal_to: Book.pre_rating_min}
+	
+	after_initialize :init
+	def init
+		self.date_added ||= Date.today
+	end
+	def to_s
+		"#{self.book} #{self.post_rating}"
+	end
 end
