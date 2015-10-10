c23186380d4b881a11144404060fe4f8f3d7e973
Allen Garvey <allengarvey@hotmail.com>
2015-10-10 19:22:28 -0400

    edit list_books controller to use before new hook to pass list id to view

--- app/controllers/list_books_controller.rb
+++ app/controllers/list_books_controller.rb
@@ -1,7 +1,7 @@
 class ListBooksController < BaseController
     protected
     def model_params
-        params.require(:list_book).permit(:book_id, :list_id)
+        params.require(:list_book).permit(:book_id, :list_id, :list)
     end
     def related_fields
         @books = Book.all
@@ -15,5 +15,8 @@ class ListBooksController < BaseController
     end
     def model_title
         'List_Book'
+    end
+    def before_render_new
+        @list_id = params[:list]
     end
 end
