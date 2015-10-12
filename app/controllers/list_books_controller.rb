dbe6952aae10d56b039d18b15456a4388cee75c0
Allen Garvey <allengarvey@hotmail.com>
2015-10-11 21:32:44 -0400

    add redirect to caller after model created and destroyed in list_books controller

--- app/controllers/list_books_controller.rb
+++ app/controllers/list_books_controller.rb
@@ -20,4 +20,17 @@ class ListBooksController < BaseController
         @list_id = params[:list].to_i
         @book_id = params[:book].to_i
     end
+    def redirect_after_model_created(model)
+        redirect_to_caller
+    end
+    def redirect_after_destroy
+        redirect_to_caller
+    end
+    def redirect_to_caller
+        if params[:list] and params[:list].to_i > 0
+            redirect_to url_for :controller => 'lists', :action => 'show', :id => params[:list].to_i
+        elsif params[:book] and params[:book].to_i > 0
+            redirect_to url_for :controller => 'books', :action => 'show', :id => params[:book].to_i
+        end
+    end
 end
