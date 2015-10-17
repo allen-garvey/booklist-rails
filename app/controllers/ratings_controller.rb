05c836dadcdf4a91b4d09b1051d8c185636743f1
Allen Garvey <allengarvey@hotmail.com>
2015-10-16 22:53:04 -0400

    edit ratings controller to allow redirect back to book after creation and pre-fill book selection when called from book

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -12,4 +12,20 @@ class RatingsController < BaseController
     def model_name
         'rating'
     end
+    def render_new
+        set_caller_params
+        super
+    end
+    def render_create_failed
+        set_caller_params
+        super
+    end
+    def set_caller_params
+        @book_id = params[:book].to_i if params[:book].to_i > 0
+    end
+    def caller_url
+        if params[:book] and params[:book].to_i > 0
+            url_for :controller => 'books', :action => 'show', :id => params[:book].to_i
+        end
+    end
 end
