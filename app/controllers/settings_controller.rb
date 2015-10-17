5ca2d5bc38b92b91306a8bf4eee2160ee88d6102
Allen Garvey <allengarvey@hotmail.com>
2015-10-16 22:14:11 -0400

    add location to settings controller fields

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,5 @@
 class SettingsController < ApplicationController
 	def fields
-		@fields = ['Author', 'Book', 'Classification', 'Genre', 'Library', 'List', 'List_Book', 'Rating']
+		@fields = ['Author', 'Book', 'Classification', 'Genre', 'Library', 'List', 'List_Book', 'Location', 'Rating']
 	end
 end
