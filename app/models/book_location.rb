ad86569e2f87f20a2b947a7e2111df660cab07fd
Allen Garvey <allengarvey@hotmail.com>
2015-10-18 15:29:37 -0400

    add function to get book call number to book_location model

--- app/models/book_location.rb
+++ app/models/book_location.rb
@@ -6,7 +6,21 @@ class BookLocation < ActiveRecord::Base
   validates :location_id, presence: true
   validates :book_id, uniqueness: {scope: :location_id}
 
+  def calculated_call_num
+  	if self.call_number
+  		self.call_number
+  	elsif self.book.author
+  		if self.book.author.last
+  			self.book.author.last
+  		else
+  			self.book.author.first
+  		end
+  	else
+  		nil
+  	end
+  end
+
   def to_s
-  	"#{self.book} #{self.location}"
+  	"#{self.book} #{self.location} - #{calculated_call_num}"
   end
 end
