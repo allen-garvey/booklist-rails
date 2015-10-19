6eb5b62f3a36134f520a6ca778a3ad6e6b890d68
Allen Garvey <allengarvey@hotmail.com>
2015-10-18 23:06:41 -0400

    create redirect route in books controller for bookshelf

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,13 @@
 class BooksController < BaseController
+    #route to display books on bookshelf
+    def bookshelf
+        bookshelf = Library.find_by name: Book.bookshelf_title
+        if bookshelf
+            redirect_to bookshelf
+        else
+            redirect_to url_for(controller: :libraries, action: :index)
+        end
+    end
     protected
     def model_params
         params.require(:book).permit(:title, :author_id, :pre_rating, :classification_id, :genre_id)
