01379ab7b18aea6b5c9676f27052e2f4466c7cda
Allen Garvey <allengarvey@hotmail.com>
2015-10-29 23:28:04 -0400

    set root route to home controller index

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get '/bookshelf', to: 'libraries#bookshelf', as: 'bookshelf'
 
   # You can have the root of your site routed with "root"
-  root 'settings#fields'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
