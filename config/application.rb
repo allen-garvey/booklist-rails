0e716fa3736f8fae484ab2345c5fb69a126251bf
Allen Garvey <allengarvey@hotmail.com>
2015-10-30 23:46:51 -0400

    edit config to load all classes in lib folder on startup

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Booklist
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #load lib directory on startup
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
