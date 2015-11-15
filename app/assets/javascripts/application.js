10a970319c2d2c48cb9f51ff73b837c3fd9f77c6
Allen Garvey <allengarvey@hotmail.com>
2015-11-15 00:04:54 -0500

    added back jquery, ujs and turbolinks because they are required for delete links to work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= dont_require jquery
-//= dont_require jquery_ujs
-//= dont_require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
