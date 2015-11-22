1574247dd54ef50a1fe564589c77aafc32cf236b
Allen Garvey <allengarvey@hotmail.com>
2015-11-21 22:24:11 -0500

    remove rails built in jquery, jquery_ujs and turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= dont_require jquery
+//= dont_require jquery_ujs
+//= dont_require turbolinks
