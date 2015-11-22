41a1a013444cbffe583a73b2a148303872850e13
Allen Garvey <allengarvey@hotmail.com>
2015-11-21 22:23:36 -0500

    add javascript to add confirm message to forms that will delete items

--- app/assets/js_source/delete_confirm_popup.js
+++ app/assets/js_source/delete_confirm_popup.js
@@ -2,18 +2,14 @@
  * Create confirm dialog box for delete and perform action on confirm
  */
 (function(){
-    var confirm_links = document.querySelectorAll('a[data-confirm]');
-    BL.each(confirm_links, function(item){
-    	item.onclick = function(event){
+    var delete_forms = document.querySelectorAll('form[data-confirm]');
+    BL.each(delete_forms, function(item){
+    	item.onsubmit = function(event){
 	    	event.preventDefault();
 	    	var message = this.getAttribute('data-confirm');
-	    	var method = this.getAttribute('data-method');
-	    	var url = this.href;
 	    	var shouldDelete = window.confirm(message);
 	    	if(shouldDelete){
-	    		BL.ajax({type : method, url : url}, function(){
-	    			window.location = url;
-	    		});
+	    		this.submit();
 	    	}
 	    };
     });
