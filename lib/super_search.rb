925183ef6de24257a2474ac78d3d6abbc647dca3
Allen Garvey <allengarvey@hotmail.com>
2015-11-21 21:21:23 -0500

    add methods to super search class to get url for specific search engine

--- lib/super_search.rb
+++ lib/super_search.rb
@@ -1,5 +1,14 @@
 class SuperSearch
+	def self.url
+		"http://localhost/super-search/"
+	end
+	def self.query_param_for(item_name)
+		item_name.gsub(/\s+/, '+')
+	end
 	def self.url_for(item_name)
-		"http://localhost/super-search/index.php?q=#{item_name.gsub(/\s+/, '+')}"
+		"#{SuperSearch.url}index.php?q=#{SuperSearch.query_param_for(item_name)}"
+	end
+	def self.url_for_engine(engine_key, item_name)
+		SuperSearch.url + engine_key + '/' + SuperSearch.query_param_for(item_name)
 	end
 end
