0ca95a207a2bffed0811872389ff90d7018e6818
Allen Garvey <allengarvey@hotmail.com>
2015-11-24 23:28:59 -0500

    add function to display alert or confirm modals

--- app/assets/js_source/modal.js
+++ app/assets/js_source/modal.js
@@ -11,14 +11,29 @@ BL.modal = {};
 	};
 
 	BL.modal.confirm = function(confirmFunc, options){
+		var modal = document.getElementById('modal');
+		modal.classList.remove('alert');
+		modal.classList.add('confirm');
 		options = options || {};
 		document.getElementById('modal_body').innerHTML = options.modal_text || "";
-		var confirm_button = document.getElementById('modal_alert_confirm');
+		var confirm_button = document.getElementById('modal_confirm_confirm');
 		confirm_button.innerHTML = options.confirm_button_text || 'Delete';
-		var cancel_button = document.getElementById('modal_alert_cancel');
+		var cancel_button = document.getElementById('modal_confirm_cancel');
 		cancel_button.innerHTML = options.cancel_button_text || 'Cancel';
 		cancel_button.onclick = BL.modal.hide;
 		confirm_button.onclick = confirmFunc;
+		BL.modal.show();
+	};
+
+	BL.modal.alert = function(options){
+		var modal = document.getElementById('modal');
+		modal.classList.remove('confirm');
+		modal.classList.add('alert');
+		options = options || {};
+		document.getElementById('modal_body').innerHTML = options.modal_text || "";
+		var confirm_button = document.getElementById('modal_alert_confirm');
+		confirm_button.innerHTML = options.confirm_button_text || 'Ok';
+		confirm_button.onclick = BL.modal.hide;
 		BL.modal.show();
 	};
 
