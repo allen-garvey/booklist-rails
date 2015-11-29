f35f278108226bb6ce3463e7b9fe62a08a6d5c0a
Allen Garvey <allengarvey@hotmail.com>
2015-11-28 19:50:08 -0500

    add qr code generation feature to display barcode modal

--- app/assets/js_source/barcode_popup.js
+++ app/assets/js_source/barcode_popup.js
@@ -7,15 +7,22 @@ BL.barcode = {};
 
 BL.barcode.show = function(){
 	var book_list = BL.closest(this, function(el){return el.className === 'show_subcategory'}).querySelector('.show_subcategory_list');
+	var book_list_html = '';
 	var book_list_string = '';
 	BL.each(book_list.children, function(){
 		var title_link = this.querySelector('.js_book_title');
 		var title = title_link ? title_link.innerHTML : '';
 		var call_num = this.querySelector('.super_search_link');
 		var call_num_string = call_num ? call_num.innerHTML : '';
-		book_list_string = book_list_string + title + ' ' + call_num_string + '<br/>';
+		book_list_html = book_list_html + title + ' ' + call_num_string + '<br/>';
+		book_list_string = book_list_string + title + ' ' + call_num_string + '\n';
 	});
-	BL.modal.alert({modal_text : book_list_string});
+	BL.modal.alert({modal_text : book_list_html});
+	new QRCode(document.getElementById("modal_body"), {
+		text : book_list_string,
+		width : 500,
+		height : 500
+	});
 };
 
 
