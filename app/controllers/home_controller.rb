d68098f51ba894d72c40882f4bd0d081093ec0e7
Allen Garvey <allengarvey@hotmail.com>
2016-01-16 23:10:56 -0500

    change homepage to use flash.now so flash message doesn't persist between requests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	def index
 		item_due_soon = LibraryItem.item_due_soon
         if item_due_soon
-        	flash[:info] = view_context.link_to "Due back soon: #{item_due_soon}", item_due_soon
+        	flash.now[:info] = view_context.link_to "Due back soon: #{item_due_soon}", item_due_soon
     	end
     end
 end
