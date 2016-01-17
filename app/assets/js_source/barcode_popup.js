409beff50b913320ab883866a7b3f05ad3b343e5
Allen Garvey <allengarvey@hotmail.com>
2016-01-17 11:13:30 -0500

    add ability to remove items from qr code popup and regenerate qr code

--- app/assets/js_source/barcode_popup.js
+++ app/assets/js_source/barcode_popup.js
@@ -7,27 +7,48 @@ BL.barcode = {};
 
 BL.barcode.show = function(){
 	var book_list = BL.closest(this, function(el){return el.className === 'show_subcategory'}).querySelector('.show_subcategory_list');
-	var book_list_html = '<ul class="qr_code_list">';
+	var book_list_html = '<ul class="qr_code_list" id="qr_code_list">';
 	var book_list_string = '';
 	BL.each(book_list.children, function(){
 		var title_link = this.querySelector('.js_book_title');
 		var title = title_link ? title_link.innerHTML : '';
 		var call_num = this.querySelector('.super_search_link');
 		var call_num_string = call_num ? call_num.innerHTML : '';
-		book_list_html = book_list_html + "<li>" + title + ' ' + call_num_string + '</li>';
-		book_list_string = book_list_string + title + ' ' + call_num_string + '\n';
+		var qr_code_string = title + ' ' + call_num_string + '\n';
+		book_list_html = book_list_html + '<li data-qr-string="' + qr_code_string + '"><a onclick="BL.barcode.removeListItem(this);">[x]</a> ' + title + ' ' + call_num_string + '</li>';
+		book_list_string = book_list_string + qr_code_string;
 	});
-	book_list_html = book_list_html + "</ul>";
+	book_list_html = book_list_html + "</ul><div id='qr_code'></div>";
 	BL.modal.alert({modal_text : book_list_html});
 	BL.barcode.generate(book_list_string);
 };
 
 BL.barcode.generate = function(code_content){
-	new QRCode(document.getElementById("modal_body"), {
+	new QRCode(document.getElementById("qr_code"), {
 		text : code_content,
 		width : 500,
 		height : 500
 	});
+};
+
+BL.barcode.getBarcodeString = function(){
+	var barcodeString = '';
+	var barcode_list = document.getElementById('qr_code_list');
+	BL.each(barcode_list.children, function(){
+		barcodeString = barcodeString + this.getAttribute('data-qr-string');
+	});
+
+	return barcodeString;
+
+};
+
+BL.barcode.removeListItem = function(caller){
+	var parent_li = BL.closest(caller, function(el){
+		return el.tagName.toLowerCase() === 'li';
+	});
+	parent_li.parentNode.removeChild(parent_li);
+	document.getElementById('qr_code').innerHTML = '';
+	BL.barcode.generate(BL.barcode.getBarcodeString());
 };
 
 
