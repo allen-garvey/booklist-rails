8bb937e87893fe435497b4c04f64cff81aeb18b7
Allen Garvey <allengarvey@hotmail.com>
2016-01-16 23:05:23 -0500

    Add flash message to homepage when library items are due soon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+	def index
+		item_due_soon = LibraryItem.item_due_soon
+        if item_due_soon
+        	flash[:info] = "Due back soon: #{item_due_soon}"
+    	end
+    end
 end
