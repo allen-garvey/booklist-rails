114e58ae0f8cd3e12a1fb262f0a80afca95ce60b
Allen Garvey <allengarvey@hotmail.com>
2016-01-16 23:34:37 -0500

    refactor qr code booklist to be unordered list instead of plain text

--- app/assets/js_source/barcode_popup.js
+++ app/assets/js_source/barcode_popup.js
@@ -7,16 +7,17 @@ BL.barcode = {};
 
 BL.barcode.show = function(){
 	var book_list = BL.closest(this, function(el){return el.className === 'show_subcategory'}).querySelector('.show_subcategory_list');
-	var book_list_html = '';
+	var book_list_html = '<ul class="qr_code_list">';
 	var book_list_string = '';
 	BL.each(book_list.children, function(){
 		var title_link = this.querySelector('.js_book_title');
 		var title = title_link ? title_link.innerHTML : '';
 		var call_num = this.querySelector('.super_search_link');
 		var call_num_string = call_num ? call_num.innerHTML : '';
-		book_list_html = book_list_html + title + ' ' + call_num_string + '<br/>';
+		book_list_html = book_list_html + "<li>" + title + ' ' + call_num_string + '</li>';
 		book_list_string = book_list_string + title + ' ' + call_num_string + '\n';
 	});
+	book_list_html = book_list_html + "</ul>";
 	BL.modal.alert({modal_text : book_list_html});
 	BL.barcode.generate(book_list_string);
 };
