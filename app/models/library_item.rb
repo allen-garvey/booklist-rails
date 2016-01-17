2077c4d637797436a5285d31d027420cdf6a63a9
Allen Garvey <allengarvey@hotmail.com>
2016-01-16 23:04:54 -0500

    add class method to library item to get library items that are due soon

--- app/models/library_item.rb
+++ app/models/library_item.rb
@@ -2,6 +2,15 @@ class LibraryItem < ActiveRecord::Base
   def self.default_order
 		self.order(due_date: :asc)
 	end
+	#returns single library item that is due soon or nil
+  	def self.item_due_soon
+  		due_soon_items = LibraryItem.where("due_date <= ?", Date.today + 3.days).order("due_date ASC").limit(1)
+        if !due_soon_items.empty?
+        	due_soon_items[0]
+        else
+        	nil
+    	end
+  	end
 	belongs_to :library
 	validates :library_id, presence: true
 	validates :due_date, presence: true
