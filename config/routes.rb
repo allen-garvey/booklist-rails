754af22e7ac21bd0c46adaedb3b6794930b327de
Allen Garvey <allengarvey@hotmail.com>
2016-01-30 20:54:38 -0500

    change bookshelf route to route to books controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  get '/bookshelf', to: 'libraries#bookshelf', as: 'bookshelf'
+  get '/bookshelf', to: 'books#bookshelf', as: 'bookshelf'
 
   # You can have the root of your site routed with "root"
   root 'home#index'
