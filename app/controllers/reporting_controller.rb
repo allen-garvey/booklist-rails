966e720dd29310b4022ed421c04e6cc50a979dcf
Allen Garvey <allengarvey@hotmail.com>
2016-02-12 20:10:06 -0500

    add variable for number of books read and make current year available to view

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -1,8 +1,9 @@
 class ReportingController < ApplicationController
 	def index
-		current_year = Time.now.year
-		@current_ratings_average = (Rating.where('extract(year from date_added) = ?', current_year).average(:post_rating) * 100).round / 100.0
-		@current_ratings_max = Rating.where('extract(year from date_added) = ?', current_year).order(post_rating: :desc).first
-		@current_ratings_min = Rating.where('extract(year from date_added) = ?', current_year).order(:post_rating).first
+		@current_year = Time.now.year
+		@current_ratings_num = Rating.where('extract(year from date_added) = ?', @current_year).count
+		@current_ratings_average = (Rating.where('extract(year from date_added) = ?', @current_year).average(:post_rating) * 100).round / 100.0
+		@current_ratings_max = Rating.where('extract(year from date_added) = ?', @current_year).order(post_rating: :desc).first
+		@current_ratings_min = Rating.where('extract(year from date_added) = ?', @current_year).order(:post_rating).first
 	end
 end
