a2c7b44066a6beb7a6101028a361727b33877efe
Allen Garvey <allengarvey@hotmail.com>
2016-02-26 21:34:27 -0500

    add query to get books read per week

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -5,5 +5,7 @@ class ReportingController < ApplicationController
 		@current_ratings_average = (Rating.where('extract(year from date_added) = ?', @current_year).average(:post_rating) * 100).round / 100.0
 		@current_ratings_max = Rating.where('extract(year from date_added) = ?', @current_year).order(post_rating: :desc).first
 		@current_ratings_min = Rating.where('extract(year from date_added) = ?', @current_year).order(:post_rating).first
+
+		@books_by_week_rows = Rating.connection.select_all "SELECT extract(week FROM date_added) :: BIGINT AS week, count(*) as books FROM ratings WHERE extract(YEAR FROM date_added) = '#{@current_year}' GROUP BY week ORDER BY week"
 	end
 end
