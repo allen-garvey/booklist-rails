98e1e779535a72ef509a5eeb3713b7edcd1fcf8d
Allen Garvey <allengarvey@hotmail.com>
2016-08-23 22:47:46 -0400

    Add list of top rated books for the year to reporting page

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -3,7 +3,8 @@ class ReportingController < ApplicationController
 		@current_year = Time.now.year
 		@current_ratings_num = Rating.where('extract(year from date_added) = ?', @current_year).count
 		@current_ratings_average = (Rating.where('extract(year from date_added) = ?', @current_year).average(:post_rating) * 100).round / 100.0
-		@current_ratings_max = Rating.where('extract(year from date_added) = ?', @current_year).order(post_rating: :desc).first
+		@top_ratings = Rating.where('extract(year from date_added) = ?', @current_year).order(post_rating: :desc, created_at: :desc).take(10)
+		@current_ratings_max = @top_ratings.first
 		@current_ratings_min = Rating.where('extract(year from date_added) = ?', @current_year).order(:post_rating).first
 
 		@books_by_week = Rating.connection.select_rows("SELECT extract(week FROM date_added) :: BIGINT AS week, count(*) as books FROM ratings WHERE extract(YEAR FROM date_added) = '#{@current_year}' GROUP BY week ORDER BY week")
