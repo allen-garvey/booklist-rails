bcbef2a80978d3fb9bae88b4645e932ee94e0622
Allen Garvey <allen.garvey@gmail.com>
2017-06-23 23:24:57 -0400

    Edit new library items to be due in 3 weeks by default

--- app/models/library_item.rb
+++ app/models/library_item.rb
@@ -12,12 +12,18 @@ class LibraryItem < ActiveRecord::Base
         	nil
     	end
   	end
+
+  	def self.default_due_date
+  		3.weeks.from_now.strftime("%Y-%m-%d")
+  	end
+
 	belongs_to :library
 	validates :library_id, presence: true
 	validates :due_date, presence: true
 	validates :due_date, uniqueness: {scope: :library_id}
 	validates :items, numericality: {only_integer: true, greater_than_or_equal_to: 1, allow_nil: true}
 	validate :due_date_cannot_be_in_the_past
+
 	def to_s
 		"#{self.library} #{DateFormatter.default_format(self.due_date)}"
 	end
@@ -27,4 +33,5 @@ class LibraryItem < ActiveRecord::Base
 			errors.add(:due_date, "can't be in the past") 
   		end
   	end
+
 end
